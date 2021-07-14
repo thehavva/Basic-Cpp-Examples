932d84c077c44f159e208a238e37e194c292a692
Havva Yılmaz <86909580+thehavva@users.noreply.github.com>
2021-07-14 21:39:18 +0300

    Add files via upload

---
+++ File Processing with c++.cpp
@@ -0,0 +1,22 @@
+﻿// File Processing with c++.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <fstream> //for file processing
+#include <string> //for getline 
+using namespace std;
+
+int main() {
+	int data;
+	string name;
+	//Declare file stream variables such as the following
+	ofstream inputfile;
+	inputfile.open("file1.txt");
+	cout << "input a name : ";
+	getline(cin, name);
+	cout << "Enter your data : ";
+	cin >> data;
+	inputfile << name << endl<<data; //Write datas to file
+	inputfile.close();  //close file
+	return 0;
+}
